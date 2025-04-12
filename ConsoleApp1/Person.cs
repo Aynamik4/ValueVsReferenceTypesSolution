0ef3c89b76c640772f371ccb7013c725354c58fa
Aynamik4 <violintailgut@gmail.com>
2025-04-12 10:18:45 +0200

    Add project files.

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace ConsoleApp1;
+
+internal class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<string> WatchCollection { get; set; }
+
+    public Person ShallowClone()
+    {
+        return new Person()
+        {
+            Name = this.Name,
+            Age = this.Age,
+            WatchCollection = this.WatchCollection
+        };
+    }
+
+    public Person DeepClone()
+    {
+        // Deep Clone
+        Person person = new()
+        {
+            Age = this.Age,
+            Name = this.Name,
+            WatchCollection = new List<string>() // or just []
+        };
+
+        foreach (string watch in WatchCollection)
+            person.WatchCollection.Add(new string(watch));
+
+        return person;
+
+        //return new Person()
+        //{
+        //    Name = this.Name,
+        //    Age = this.Age,
+        //    WatchCollection = this.WatchCollection.Select(w => new string(w)).ToList()
+        //};
+    }
+
+    public Person JSON_DeepClone()
+    {
+        // Deep Clone for DTOs
+        string json = JsonSerializer.Serialize(this);
+        return JsonSerializer.Deserialize<Person>(json);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new(1024);
+
+        sb.Append($"{Name}{Environment.NewLine}{Age}");
+
+        foreach (string watch in WatchCollection)
+            sb.Append($"{Environment.NewLine}{watch}");
+
+        return sb.ToString();
+
+        //string s = $"{Name}{Environment.NewLine}{Age}";
+        //foreach (string watch in WatchCollection)
+        //    s += $"{Environment.NewLine}{watch}";
+        //return s;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int i = 10;
+            int j = i;
+            i = 20;
+            Console.WriteLine($"i: {i}");
+            Console.WriteLine();
+            Console.WriteLine($"j: {j}");
+            Console.WriteLine("-------------------------------");
+
+            Person me = new() { Name = "Håkan", Age = 62, WatchCollection = ["Audemars Piguet", "Vacheron Constantin", "Patek Philippe"] };
+
+            Person you = me;
+            //Person you = me.ShallowClone();
+            //Person you = me.DeepClone();
+            //Person you = me.JSON_DeepClone();
+
+            you.Name = "Pontus";
+            you.Age = 38;
+            you.WatchCollection[0] = "Seiko";
+            you.WatchCollection[1] = "Citizen";
+            you.WatchCollection[2] = "Casio";
+
+            Console.WriteLine($"me:{Environment.NewLine}{me}");
+            Console.WriteLine();
+            Console.WriteLine($"you:{Environment.NewLine}{you}");
+        }
+    }
+}
